5e375a745b66d6040678fa581be82361f1dc45c9
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-04-20 19:05:38 +0530

    Update FunctionOverloading.cpp

--- OOPS/FunctionOverloading.cpp
+++ OOPS/FunctionOverloading.cpp
@@ -1,1 +1,15 @@
+/* Write a program to compute absolute 
+and average value for integer and
+floating point numbers */
 
+#include<iostream>
+using namespace std;
+
+class compute{
+  public:
+  int abs(int);
+  float abs(float);
+  float avg(int,float);
+  float avg(int,int);
+  float avg(float,float);
+};
