ee449a889bf121fd6a7819b6a4326746506d518d
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-05-16 16:56:04 +0530

    Update constructors.cpp

--- OOPS/constructors.cpp
+++ OOPS/constructors.cpp
@@ -31,3 +31,49 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+//Definition outside class:
+
+#include <iostream>
+
+using namespace std;
+
+class Test{
+  private :
+  int n;
+  public:
+  Test();
+  Test(int);
+  void display();
+};
+
+Test::Test():n(10)
+{
+    cout<<"Constructor called"<<endl;
+    }
+    
+Test::Test(int num){
+    n=num;
+}
+
+void Test :: display(){
+      cout<<"Value of n: "<<n<<endl;
+  }
+
+
+
+int main()
+{
+   Test t1;
+   Test t2(20);
+   t1.display();
+   t2.display();
+
+    return 0;
+}
+
