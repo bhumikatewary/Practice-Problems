62d22896cd84e61c0d08014a84b25ca25e1209ef
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-05-18 00:19:26 +0530

    Update Object.cpp

--- OOPS/Object.cpp
+++ OOPS/Object.cpp
@@ -1,1 +1,36 @@
 /* Object creation, count and deletion */
+
+
+#include <iostream>
+using namespace std;
+
+class count{
+  public:
+  static int counter;
+  count();
+  void display();
+  ~count();
+  
+};
+
+int count::counter=0;
+
+count::count(){
+    counter++;
+}
+
+void count::display(){
+    cout<<"Object created: "<<counter<<endl;
+}
+
+count::~count(){
+    counter--;
+    cout<<"Object destroyed: "<<counter<<endl;
+}
+
+int main()
+{
+   count c1,c2,c3,c4,c5;
+   c1.display();
+    return 0;
+}
