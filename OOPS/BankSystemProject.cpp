1a240e706e4e6bccac60c684350df77444caa3b5
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-05-18 00:33:16 +0530

    Create BankSystemProject.cpp

---
+++ OOPS/BankSystemProject.cpp
@@ -0,0 +1,12 @@
+/* WAP that define a class for a bank account that includes following data:
+1. Name of depositor
+2. Account name
+3. Balance amount in the Account
+
+The class count has the following member functions:
+1. A constructor to assign initial values
+2. Deposit function to deposit some amount. It accepts amount as a parameter
+3. Withdraw function to withdraw some amount after checking the balance(accepts amount as parameter)
+4. Display function to display name and balance
+
+*/
