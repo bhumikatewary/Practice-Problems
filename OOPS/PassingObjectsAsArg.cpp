e80d8d959ea4f412192b531401cc06a6373b6896
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-05-19 21:22:31 +0530

    Update PassingObjectsAsArg.cpp

--- OOPS/PassingObjectsAsArg.cpp
+++ OOPS/PassingObjectsAsArg.cpp
@@ -51,3 +51,68 @@ int main()
 
     return 0;
 }
+
+
+
+
+////////////////////// OR ////////////////////////////////
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Distance{
+    private:
+    int km,hr;
+    public:
+    Distance();
+    void getValue();
+    void display();
+    Distance totalDistance(Distance);
+};
+
+Distance::Distance():km(0),hr(0){
+}
+
+void Distance::getValue(){
+    cout<<"Enter the distance travelled in km"<<endl;
+    cin>>km;
+    cout<<"Enter the hours taken for travel"<<endl;
+    cin>>hr;
+}
+
+void Distance::display(){
+    cout<<"The travelled distance is: "<<km<<" kilometers"<<endl;
+    cout<<"The travelled hours are :"<<hr<<" hours"<<endl;
+}
+
+Distance Distance::totalDistance(Distance pass){
+    Distance t;
+    t.km = km + pass.km;
+    t.hr = hr + pass.hr;
+    return t;
+    //cout<<"The total distance is: "<<t.km<<" km travelled in "<<t.hr<<" hours"<<endl;
+}
+
+
+int main()
+{
+    
+    Distance d1;
+    Distance d2;
+    Distance res;
+    cout<<"============Source=========== "<<endl;
+    d1.getValue();
+    d1.display();
+    cout<<"=========Destination============  "<<endl;
+    d2.getValue();
+    d2.display();
+    res = d1.totalDistance(d2);
+    cout<<"=========TOTAL DISTANCE AND HOURS=========="<<endl;
+    res.display();
+    
+    
+
+    return 0;
+}
+
