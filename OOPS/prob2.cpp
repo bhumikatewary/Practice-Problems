932f668a7c2f90caa2e4f940c228258084771efe
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-06-14 18:35:27 +0530

    Create prob2.cpp

---
+++ OOPS/prob2.cpp
@@ -0,0 +1,4 @@
+//Write a class Teacher that contains the attribute teacher name, age and address. It also contains member funtions input and show for its attributes.
+//Write a class Student that contains the attribute student name, gender and total marks obtained in all subjects. It also contains member functions to input and show its 
+//attributes.
+//Write third class Scholar that inherits both Teacher and student class.
