642ee28fd56c56dcb2e6080a247dea259db52250
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-06-14 17:38:24 +0530

    Create prob1.cpp

---
+++ OOPS/prob1.cpp
@@ -0,0 +1,3 @@
+// Write a class Book that contains the attribute bookID, book name and price. It also contains member functions to input and show its attributes. 
+//Write another class Writer that contains the attributes writer name, address and number of books written by him. It contains total 5 books as objects. It also contains
+//member function to input and show its attributes.
